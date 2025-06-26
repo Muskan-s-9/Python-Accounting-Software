fe60cb284495d93f4901d4a239535bb1687d6b9a
Muskan Sonkar <87746861+Muskan-s-9@users.noreply.github.com>
2025-06-26 19:22:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,23 +7,21 @@ from generate_report.generate_detail_balance_sheets import generate_detail_balan
 from generate_report.generate_ledger_pivot import generate_ledger_pivot
 from generate_report.generate_pnl_report import generate_pnl_report
 
-
+#scrap the accounting data from the invoices
 process_invoices_with_rag_and_cache()
 
-# def passing_journal_entry(entry_type):
-#     PassingJournalEntry(entry_type).passing_entry()
+#passing sales,purchase,credit_note,debit_note journal entry
+def passing_journal_entry(entry_type):
+    PassingJournalEntry(entry_type).passing_entry()
 
-# Main guard block
-#if __name__ == "__main__":
+passing_journal_entry('sales')
+passing_journal_entry('purchase')
+passing_journal_entry('credit_note')
+passing_journal_entry('debit_note')
 
-
-# passing_journal_entry('sales')
-# passing_journal_entry('purchase')
-# passing_journal_entry('credit_note')
-# passing_journal_entry('debit_note')
-
-# merge_excel_files()
-# generate_balance_sheet()
-# generate_detail_balance_sheets()
-# generate_ledger_pivot()
-# generate_pnl_report()
+#generate balance sheet
+merge_excel_files()
+generate_balance_sheet()
+generate_detail_balance_sheets()
+generate_ledger_pivot()
+generate_pnl_report()
